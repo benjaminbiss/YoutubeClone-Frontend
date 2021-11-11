0db47f475a14ada25bb897189728201fca193fb5
Ben Bissett <benjaminbissett@gmail.com>
2021-11-11 10:07:41 -0700

    Added a get function that requests the video title and description from the youtube api and displays the information underneath the embeded video.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import { googleapikey } from './keys';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
 
-
+    const [video, setVideo] = useState();
+    const [videoID, setID] = useState();
+    const [videoTitle, setTitle] = useState();
+    const [videoDetail, setDetail] = useState();
+    
+    // variables: take out api key and video id
+    useEffect(async () => {
+        console.log("Noticed Change of video?");
+        let result = await axios.get(`https://www.googleapis.com/youtube/v3/videos?key=${googleapikey}&part=snippet&type=video&id=DxfEbulyFcY`)
+        setVideo(result.data.items[0])
+        setTitle(result.data.items[0].snippet.title)
+        setDetail(result.data.items[0].snippet.description)
+    }, [])
+    
 
     return (
         <div>
             <h1>Youtube Clone</h1>
+            {/* <SearchBar /> */}
             <div>
-            <iframe width="800px" height="600px" src="https://www.youtube.com/embed/DxfEbulyFcY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="800px" height="600px" src="https://www.youtube.com/embed/DxfEbulyFcY" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <h2>Video: {videoTitle}</h2>
+            <p><strong>Details:</strong> {videoDetail}</p>
             </div>
         </div>
     );
